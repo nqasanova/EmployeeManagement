74b06851bbba94211d14bf35e24acee1a0c99c11
Natavan Hasanova <natavanhesenova4@gmail.com>
2022-11-20 22:28:11 +0400

    Validations created and added to the system.

---
+++ EmployeeManagement/EmployeeManagement/Validations/FINValidation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace EmployeeManagement.Validations
+{
+    public class FINValidation : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+
+            Regex regex = new Regex(@"^[A-Z0-9]{7}$");
+
+            if (value != null)
+            {
+                string FIN = value.ToString();
+                if (regex.IsMatch(FIN))
+                {
+                    return ValidationResult.Success;
+                }
+            }
+            return new ValidationResult("Employee's FIN is incorrect!");
+        }
+    }
+}
