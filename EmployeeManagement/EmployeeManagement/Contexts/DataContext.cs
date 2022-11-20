6c3d1af490846c4e244e426961219db4887b77a8
Natavan Hasanova <natavanhesenova4@gmail.com>
2022-11-20 22:23:27 +0400

    Contexts Folder created and added to the system.

---
+++ EmployeeManagement/EmployeeManagement/Contexts/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using EmployeeManagement.Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Contexts
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=EmployeeManagement;Trusted_Connection=false;User=SA;Password=Nata2004.;TrustServerCertificate=True;");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeManagement/EmployeeManagement/Contexts/EmployeePKGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using EmployeeManagement.Database.Models;
+
+namespace EmployeeManagement.Contexts
+{
+    public class EmployeePKGenerator
+    {
+        static Random _random = new Random();
+        private static string _employeeCode;
+
+        public static string EmployeeCode
+        {
+            get
+            {
+                DataContext dataContext = new DataContext();
+                var employees = dataContext.Employees.ToList();
+
+                bool status = true;
+                string _employeeCode = "E" + _random.Next(10000, 100000);
+
+                foreach (var employee in employees)
+                {
+                    if (employee.EmployeeCode == _employeeCode)
+                    {
+                        do
+                        {
+                            _employeeCode = "E" + _random.Next(10000, 100000);
+
+                        } while (employee.EmployeeCode != _employeeCode);
+                    }
+
+
+                }
+                return _employeeCode;
+            }
+        }
+    }
+}
