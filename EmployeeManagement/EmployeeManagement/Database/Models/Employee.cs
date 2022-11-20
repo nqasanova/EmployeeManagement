875a191405da2962153d26e3a83c8905e072dde7
Natavan Hasanova <natavanhesenova4@gmail.com>
2022-11-20 22:25:34 +0400

    Database created and added to the system.

---
+++ EmployeeManagement/EmployeeManagement/Database/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace EmployeeManagement.Database.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string EmployeeCode { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FatherName { get; set; }
+        public string Email { get; set; }
+        public string FIN { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
